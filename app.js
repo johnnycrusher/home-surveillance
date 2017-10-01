e2cc1b4135f5743b2d07fdfd5a557dd519c1f878
John Huynh <John.huynh@student.qut.edu.au>
2017-10-01 21:34:15 +1000

    Updated with HTTPS

--- app.js
+++ app.js
@@ -4,10 +4,27 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var https = require('https');
+var http = require('http');
+var fs = require('fs');
 
 var index = require('./routes/index');
+// This line is from the Node.js HTTPS documentation.
+
+// This line is from the Node.js HTTPS documentation.
+var options = {
+  key: fs.readFileSync('192.168.1.94.key'),
+  cert: fs.readFileSync('192.168.1.94.crt')
+};
+
 
 var app = express();
+https.createServer(options, app).listen(8000);
+
+// Create an HTTP service.
+http.createServer(app).listen(80);
+// Create an HTTPS service identical to the HTTP service.
+// https.createServer(options, app).listen(443);
 
 // view engine setup
 // app.set('views', path.join(__dirname, 'views'));
