d9217bf2a0269229baac9154f65801f39ecb76e5
John Huynh <John.huynh@student.qut.edu.au>
2017-10-07 02:04:26 +1000

    Added Encryption and Decryption Scripts

---
+++ app/pictures/Decrypt_file.py
@@ -0,0 +1,38 @@
+import os, random, struct
+import time
+from Crypto.Cipher import AES
+
+def decrypt_file(key, in_filename, out_filename=None, chunksize=24*1024):
+    """ Decrypts a file using AES (CBC mode) with the
+        given key. Parameters are similar to encrypt_file,
+        with one difference: out_filename, if not supplied
+        will be in_filename without its last extension
+        (i.e. if in_filename is 'aaa.zip.enc' then
+        out_filename will be 'aaa.zip')
+    """
+    if not out_filename:
+        out_filename = os.path.splitext(in_filename)[0]
+
+    with open(in_filename, 'rb') as infile:
+        origsize = struct.unpack('<Q', infile.read(struct.calcsize('Q')))[0]
+        iv = infile.read(16)
+        decryptor = AES.new(key, AES.MODE_CBC, iv)
+
+        with open(out_filename, 'wb') as outfile:
+            while True:
+                chunk = infile.read(chunksize)
+                if len(chunk) == 0:
+                    break
+                outfile.write(decryptor.decrypt(chunk))
+
+            outfile.truncate(origsize)
+
+FileName = 'FaceCap';
+number = 0;
+File_extension = ".png"
+Encrption_extension = ".enc"
+while(1):
+	time.sleep(0.5);
+	if(os.path.isfile(FileName+str(number)+File_extension+Encrption_extension)):
+		decrypt_file("1234567891234567",FileName+str(number)+File_extension+Encrption_extension,FileName+str(number)+File_extension,24*1024)
+		number +=1;
